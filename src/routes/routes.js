656ddf4cc881f1d3d879398180350ceec5da0b26
Matin <misaxanli@gmail.com>
2026-05-17 07:13:33 +0200

    feat: add ROUTES configuration and fetchWithSignal utility for API requests with abort support

---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  BASE_NAME: "/Open-Library-Search-API/",
+  HOME: "/",
+  SEARCH_BOOKS: "/search-books",
+  SEARCH_AUTHORS: "/search-authors",
+  AUTHOR_DETAILS: "/author/:author_key",
+  NOT_FOUND: "*",
+};
